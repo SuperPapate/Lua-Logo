10f945b8a464e2e34496676688b0e18ec172edbf
Mr Patate <SuperPatateQc@gmail.com>
2013-06-09 13:13:05 -0400

    add resouce lua

---
+++ lua/autorun/resource.lua
@@ -0,0 +1,29 @@
+
+resource.AddFile( "materials/lua-logo/32/Lua-1.png" )
+resource.AddFile( "materials/lua-logo/32/Lua-2.png" )
+resource.AddFile( "materials/lua-logo/32/Lua-3.png" )
+
+resource.AddFile( "materials/lua-logo/64/Lua.png" )
+resource.AddFile( "materials/lua-logo/64/Lua-1.png" )
+resource.AddFile( "materials/lua-logo/64/Lua-2.png" )
+resource.AddFile( "materials/lua-logo/64/Lua-3.png" )
+
+resource.AddFile( "materials/lua-logo/128/Lua.png" )
+resource.AddFile( "materials/lua-logo/128/Lua-1.png" )
+resource.AddFile( "materials/lua-logo/128/Lua-2.png" )
+resource.AddFile( "materials/lua-logo/128/Lua-3.png" )
+
+resource.AddFile( "materials/lua-logo/256/Lua.png" )
+resource.AddFile( "materials/lua-logo/256/Lua-1.png" )
+resource.AddFile( "materials/lua-logo/256/Lua-2.png" )
+resource.AddFile( "materials/lua-logo/256/Lua-3.png" )
+
+resource.AddFile( "materials/lua-logo/512/Lua.png" )
+resource.AddFile( "materials/lua-logo/512/Lua-1.png" )
+resource.AddFile( "materials/lua-logo/512/Lua-2.png" )
+resource.AddFile( "materials/lua-logo/512/Lua-3.png" )
+
+resource.AddFile( "materials/lua-logo/1024/Lua.png" )
+resource.AddFile( "materials/lua-logo/1024/Lua-1.png" )
+resource.AddFile( "materials/lua-logo/1024/Lua-2.png" )
+resource.AddFile( "materials/lua-logo/1024/Lua-3.png" )
